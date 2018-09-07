4df2da3e013d46d8b152e57a704f3f3e9e219bab
jackb333 <42563271+jackb333@users.noreply.github.com>
2018-09-07 11:09:57 -0500

    Add files via upload
    fixed numDigits  and modulus variable names from numberOfDigits and modulous. Changed 'return false' to 'return 0'.

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -5,27 +5,27 @@
 /**
  * <p>This function tests whether the given input <code>n</code>
  * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
- * integer n whose square can be split into two positive integers 
- * whose sum is equal to the original number.  For the purposes of 
+ * integer n whose square can be split into two positive integers
+ * whose sum is equal to the original number.  For the purposes of
  * this method, we restrict our consideration to base-10 representations.</p>
- * 
- * For example, 45 is a Kaprekar number, because 
- *   45<sup>2</sup> = 2025 
- * and 
- *   20 + 25 = 45. 
- * 
- * <p>See Wikipedia's  
+ *
+ * For example, 45 is a Kaprekar number, because
+ *   45<sup>2</sup> = 2025
+ * and
+ *   20 + 25 = 45.
+ *
+ * <p>See Wikipedia's
  * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
  * article for more information.</p>
- * 
+ *
  * @param n
- * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
+ * @return <code>true</code> if <code>n</code> is a Kaprekar Number,
  * <code>false</code> otherwise.
  */
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
@@ -37,9 +37,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -53,5 +53,5 @@ int isKaprekar(int n) {
     }
   }
   return 0;
-  
+
 }
