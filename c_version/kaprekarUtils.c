bd657c4e76f27fd77189abcc6d8423d247d0d4c9
rmacdonald00 <42583647+rmacdonald00@users.noreply.github.com>
2018-09-07 19:53:24 -0500

    Update kaprekarUtils.c
    line 51: should have == instead of = assignment symbol

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -48,7 +48,7 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
