54cfa88495ff289093da61999b91091fdc0bc7d8
rmacdonald00 <42583647+rmacdonald00@users.noreply.github.com>
2018-09-07 19:48:23 -0500

    Update kaprekarUtils.c
    modulus was spelled wrong

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -39,7 +39,7 @@ int isKaprekar(int n) {
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
