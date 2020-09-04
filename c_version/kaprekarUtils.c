4926fd445a81911a9a12ef4e447f3fec4ede6319
Erica Fenn <efenn2@huskers.unl.edu>
2020-09-04 21:33:36 +0000

    Changed modulus = 0 to modulus = 1

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,7 +33,7 @@ int isKaprekar(int n) {
   long square = n * (long) n;
   //compute the number of digits of the square
   int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
