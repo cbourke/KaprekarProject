9dca8ec5e1217fbcae2b4ddc12def2989d33521c
Erica Fenn <efenn2@huskers.unl.edu>
2020-09-04 21:27:02 +0000

    Change false to 0, changed numberOfDigits to numDigits, and changes modulous to modulus

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -25,7 +25,7 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
@@ -37,9 +37,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -48,7 +48,7 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
